924fbbfaf1445a4fb4a634ac3c2751b37340a2d8
Ibrahim <baim008@gmail.com>
2018-09-10 18:12:48 +0800

    added login and register page, have not put logic

--- App.js
+++ App.js
@@ -1,22 +1,231 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator, Image, ImageBackground, Dimensions,TextInput, TouchableOpacity } from 'react-native';
+import {createStackNavigator,createBottomTabNavigator,createDrawerNavigator} from 'react-navigation';
+import { Constants } from 'expo';
+import Icon from 'react-native-vector-icons/EvilIcons';
 
-export default class App extends React.Component {
+const CONST_HEIGHT = Dimensions.get('window').height;
+const CONST_WIDTH = Dimensions.get('window').width;
+
+import loginimage from './asset/image/login.jpg';
+import iconimage from './asset/image/icon.png';
+
+
+class RegisterPage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  _back = () => {
+    this.props.navigation.goBack(null);
+  }
+
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+    return(
+      <View>
+        <ImageBackground style={{width:CONST_WIDTH,height:CONST_HEIGHT}}source={loginimage}>
+
+
+
+          <View style={{backgroundColor: 'rgba(0,0,0,0.85)', height: CONST_HEIGHT,width:CONST_WIDTH, flex:1,justifyContent:'center',alignItems:'center'}}>
+
+          <View style={{alignSelf:'stretch',justifyContent:'flex-start'}}>
+            <TouchableOpacity onPress={this._back}>
+              <Icon style={{paddingLeft: 10,}} name="chevron-left" size={48} color="#91999c" />
+            </TouchableOpacity>
+          </View>
+
+
+            <View style={{alignSelf: 'stretch', margin: 3, padding: 10,}}>
+              <View style={{flexDirection: 'row',height: 45, padding:10, paddingLeft: 10, marginBottom:1, backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: 35,}}>
+                <Icon style={{paddingLeft: 10,}} name="user" size={30} color="#91999c" />
+                <TextInput underlineColorAndroid='transparent' style={{fontSize: 18,paddingLeft: 10,flex:1,color: 'white'}} placeholder='Email'></TextInput>
+              </View>
+            </View>
+
+            <View style={{alignSelf: 'stretch', margin: 3, padding: 10,}}>
+              <View style={{flexDirection: 'row',height: 45, padding:10, paddingLeft: 10, marginBottom:1, backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: 35,}}>
+                <Icon style={{paddingLeft: 10,}} name="lock" size={30} color="#91999c" />
+                <TextInput secureTextEntry={true} underlineColorAndroid='transparent' style={{fontSize: 18,paddingLeft: 10,flex:1,color: 'white'}} placeholder='Password'></TextInput>
+              </View>
+            </View>
+
+            <View style={{alignSelf: 'stretch', margin: 3, padding: 10,}}>
+              <View style={{flexDirection: 'row',height: 45, padding:10, paddingLeft: 10, marginBottom:1, backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: 35,}}>
+                <Icon style={{paddingLeft: 10,}} name="lock" size={30} color="#91999c" />
+                <TextInput secureTextEntry={true} underlineColorAndroid='transparent' style={{fontSize: 18,paddingLeft: 10,flex:1,color: 'white'}} placeholder='Confirm Password'></TextInput>
+              </View>
+            </View>
+
+            <TouchableOpacity style={{alignSelf: 'stretch', margin: 20,marginTop: 1,marginBottom: 1,padding: 12,backgroundColor: 'blue',borderWidth: 1,borderColor: '#fff',borderRadius: 35,backgroundColor: 'rgba(255, 255, 255, 0.6)',}}>
+              <Text style={{fontSize: 18,fontWeight: 'bold',textAlign: 'center',color: '#0c7d9f'}}> REGISTER </Text>
+            </TouchableOpacity>
+
+
+          </View>
+
+        </ImageBackground>
       </View>
-    );
+
+    )
+  }
+}
+class LoginPage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  _register = () => {
+    this.props.navigation.navigate("RegisterPage");
+  }
+
+
+
+  render() {
+    return(
+      <View>
+        <ImageBackground style={{width:CONST_WIDTH,height:CONST_HEIGHT}}source={loginimage}>
+
+          <View style={{backgroundColor: 'rgba(0,0,0,0.85)', height: CONST_HEIGHT,width:CONST_WIDTH, flex:1,justifyContent:'center',alignItems:'center'}}>
+
+            <View style={{justifyContent: 'center', alignItems: 'center'}}>
+              <Image style={{height:120, width:120}} source= {iconimage} />
+            </View>
+
+
+            <View style={{alignSelf: 'stretch', margin: 3, padding: 10,}}>
+              <View style={{flexDirection: 'row',height: 45, padding:10, paddingLeft: 10, marginBottom:1, backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: 35,}}>
+                <Icon style={{paddingLeft: 10,}} name="user" size={30} color="#91999c" />
+                <TextInput underlineColorAndroid='transparent' style={{fontSize: 18,paddingLeft: 10,flex:1,color: 'white'}} placeholder='Email'></TextInput>
+              </View>
+            </View>
+
+            <View style={{alignSelf: 'stretch', margin: 3, padding: 10,}}>
+              <View style={{flexDirection: 'row',height: 45, padding:10, paddingLeft: 10, marginBottom:1, backgroundColor: 'rgba(255, 255, 255, 0.3)', borderRadius: 35,}}>
+                <Icon style={{paddingLeft: 10,}} name="lock" size={30} color="#91999c" />
+                <TextInput secureTextEntry={true} underlineColorAndroid='transparent' style={{fontSize: 18,paddingLeft: 10,flex:1,color: 'white'}} placeholder='Password'></TextInput>
+              </View>
+            </View>
+
+            <TouchableOpacity style={{alignSelf: 'stretch', margin: 20,marginTop: 1,marginBottom: 1,padding: 12,backgroundColor: 'blue',borderWidth: 1,borderColor: '#fff',borderRadius: 35,backgroundColor: 'rgba(255, 255, 255, 0.6)',}}>
+              <Text style={{fontSize: 18,fontWeight: 'bold',textAlign: 'center',color: '#0c7d9f'}}> LOGIN </Text>
+            </TouchableOpacity>
+
+            <View style={{flexDirection:'row', alignSelf:'stretch', margin:20, justifyContent:'space-between'}}>
+              <TouchableOpacity onPress={this._register}>
+                <Text style={{color:'white'}}>Create Account</Text>
+              </TouchableOpacity>
+              <TouchableOpacity>
+                <Text style={{color:'white'}}>Forgot Password</Text>
+              </TouchableOpacity>
+            </View>
+
+            <View style={{flexDirection:'row', alignSelf:'stretch', margin:20, justifyContent:'flex-end'}}>
+              <TouchableOpacity>
+                <Text style={{color:'white'}}>Skip</Text>
+              </TouchableOpacity>
+            </View>
+
+
+
+
+          </View>
+
+        </ImageBackground>
+      </View>
+    )
+  }
+}
+const RegisterAndLogin = createStackNavigator(
+  {
+    LoginPage: {screen:LoginPage,},
+    RegisterPage: {screen:RegisterPage,},
+  },
+  {
+    headerMode: 'none'
+  }
+)
+
+class Tab1 extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <View>
+      <Text>This is homepage</Text>
+      </View>
+    )
+  }
+}
+class Tab2 extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <View>
+      <Text>This is homepage</Text>
+      </View>
+    )
+  }
+}
+class Tab3 extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return(
+      <View>
+      <Text>This is homepage</Text>
+      </View>
+    )
   }
 }
 
+const TabHomePage = createBottomTabNavigator(
+  {
+    Tab1: {screen: Tab1,},
+    Tab2: {screen: Tab2,},
+    Tab3: {screen: Tab3,},
+  }
+)
 
+class LoadingStackNav extends React.Component{
+  constructor(props){
+    super(props);
 
+    //Change the login status for different logic
+    this.state = {
+      login: false,
+    }
 
+  }
+  render() {
 
+    if(this.state.login == false){
+      return <RegisterAndLogin/>
+    }
+
+    if(this.state.login == true){
+      return <TabHomePage/>
+    }
+
+    return(
+      <View><Text>hmmm...</Text>
+      </View>
+    )
+  }
+}
+export default createStackNavigator(
+  {
+    LoadingStackNav: {screen: LoadingStackNav,},
+  },
+  {
+    headerMode:'none',
+  }
+)
 
 
 
